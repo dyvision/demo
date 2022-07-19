1a1856f606f6d863e0ba984005b4e9caaac8f0be
Dyvision <thedyvision@gmail.com>
2022-07-18 21:01:28 -0400

    test
    test

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ app = Flask(__name__)
 @app.route('/',methods=['GET'])
 def hello():
     return "<h1>hello {}</h1>".format(username)
+
+@app.route('/test')
+def test():
+    final = 14 + int(request.args['number'])
+    return final
